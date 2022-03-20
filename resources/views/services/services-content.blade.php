8718d565ddcbadac22ff9b1d32541601d8a3ef42
Mohamad Kabalane <mkabalane@gmail.com>
2022-03-20 22:20:04 +0200

    fix

--- resources/views/services/services-content.blade.php
+++ resources/views/services/services-content.blade.php
@@ -3,12 +3,12 @@
 <div class="col-lg-4 col-md-6">
     <div class="single-services">
         <div class="services-image">
-            <a href="/services-details/{{$row->id}}"><img src="{{ asset( 'storage/'.str_replace('\\', '/', $row->image)) }}" alt="{{$row->title_de}}"></a>
+            <a href="/service-details/{{$row->id}}"><img src="{{ asset( 'storage/'.str_replace('\\', '/', $row->image)) }}" alt="{{$row->title_de}}"></a>
         </div>
 
         <div class="services-content">
             <h3>
-                <a href="/services-details/{{$row->id}}">@if($lang=='de') {{$row->title_de}} @else {{$row->title_ar}}  @endif</a>
+                <a href="/service-details/{{$row->id}}">@if($lang=='de') {{$row->title_de}} @else {{$row->title_ar}}  @endif</a>
             </h3>
             <p>@if($lang=='de') {{ substr($row->text_de, 0, 100) }} @else {{ substr($row->text_ar, 0, 100) }}  @endif</p>
             <a href="/service-details/{{$row->id}}" class="read-more-btn">{{ __('translation.learn-more') }} +</a>
